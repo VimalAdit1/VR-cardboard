c609c2b231e635caffd115527ca8ca451f205b63
VimalAdit1 <vimalcuber@gmail.com>
2023-02-13 04:17:01 +0530

    Basic Gameplay

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [SerializeField] Text Score;
+    [SerializeField] float spawnInterval;
+    [SerializeField] UFO UFOPrefab;
+    [SerializeField] GameObject Player;
+
+    float lastSpawnTime;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        score = 0;
+        lastSpawnTime = 0f;
+        //UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        Score.text = score.ToString();
+    }
+
+    public void UFODestroyed()
+    {
+        score++;
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastSpawnTime > spawnInterval)
+        {
+            UFO ufo = GameObject.Instantiate(UFOPrefab);
+            ufo.transform.position = UnityEngine.Random.onUnitSphere * 15f;
+            ufo.transform.position += Vector3.up*15f; 
+            ufo.player = Player.transform;
+            lastSpawnTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour
+{
+    public Transform player;
+    float speed = 1f;
+    float step;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+    }
+    public void Destroy()
+
+    {
+        GameManager.Instance.UFODestroyed();
+        GameObject.Destroy(this.gameObject);
+    }
+}
