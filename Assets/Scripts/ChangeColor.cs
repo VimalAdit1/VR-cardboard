be4de53a5d0d59d221c1a6bd3b02e568a6ffafa7
dhanuvanth <dhanuvanth@gmail.com>
2023-02-12 22:25:10 +0530

    initial

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void ColorBlue() => GetComponent<MeshRenderer>().material.color = Color.blue;
+    public void ColorRed() => GetComponent<MeshRenderer>().material.color = Color.red;
+    public void Colorgreen() => GetComponent<MeshRenderer>().material.color = Color.green;
+}
